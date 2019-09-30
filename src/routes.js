5953f9ff9b213d3c7ed8ae148f188a742aef3638
Arthur <arthurbiasibettifarias@gmail.com>
2019-09-30 17:21:50 -0300

    Pega aluno da turma v2.0

--- src/routes.js
+++ src/routes.js
@@ -442,7 +442,7 @@ routes.post('/pessoa', (req, res, next) => {
 
   routes.get('/turmas_alunoProfessor/:codTurma', (req, res, next) => {
     const {codTurma} = req.params;
-    knex.raw(`select cod_turma, nome from "ALUNO4M02".tb_turma_aluno, "ALUNO4M02".tb_pessoa where id_aluno = id_pessoa and cod_turma = ${codTurma}`)
+    knex.raw(`select cod_turma, nome from "ALUNO4M02".tb_turma_aluno, "ALUNO4M02".tb_pessoa where id_aluno = id_pessoa and cod_turma = '${codTurma}'`)
     .then((dados) => {
       if(!dados) return res.send(("Nada foi encontrado"));
       res.send(dados);
