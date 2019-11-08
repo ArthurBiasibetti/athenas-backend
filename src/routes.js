fcf52d615ac75fab878d7249b6f27b8a536a10df
Arthur <arthurbiasibettifarias@gmail.com>
2019-11-08 18:41:31 -0300

    envio arquivo

--- src/routes.js
+++ src/routes.js
@@ -129,7 +129,7 @@ routes.post('/pessoa', (req, res, next) => {
     knex('ALUNO4M02.tb_arquivo')
     .then((dados) => {
       res.send(dados);
-  }, next);
+  }, next).catch(err => res.send(err));
   });
   
   routes.get('/arquivos/:id', (req, res, next) => {
